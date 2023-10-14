9b3b3601f2be8cfe4f18aca6a99cd6f0717d640a
DennisMenn <dennis2722801@gmail.com>
2023-10-14 11:03:50 -0500

    update config default values

--- run_nerf.py
+++ run_nerf.py
@@ -536,13 +536,13 @@ def config_parser():
     # logging/saving options
     parser.add_argument("--i_print",   type=int, default=100, 
                         help='frequency of console printout and metric loggin')
-    parser.add_argument("--i_img",     type=int, default=500, 
-                        help='frequency of tensorboard image logging')
+    # parser.add_argument("--i_img",     type=int, default=500, 
+    #                     help='frequency of tensorboard image logging')
     parser.add_argument("--i_weights", type=int, default=10000, 
                         help='frequency of weight ckpt saving')
-    parser.add_argument("--i_testset", type=int, default=50000, 
+    parser.add_argument("--i_testset", type=int, default=5000, 
                         help='frequency of testset saving')
-    parser.add_argument("--i_video",   type=int, default=50000, 
+    parser.add_argument("--i_video",   type=int, default=10000, 
                         help='frequency of render_poses video saving')
     parser.add_argument("--eval_test", type=bool, default=True, 
                         help='eval similarity matrix with test image saving')
